0ddf85bc91d87f1620b69f6930eeedd43d0b7c56
onlycaffeine <60136864+onlycaffeine@users.noreply.github.com>
2022-04-29 11:53:29 +0700

    fix r

--- Thuan_01.py
+++ Thuan_01.py
@@ -197,7 +197,7 @@ class Thuan_01():
                 #data1 = link + data00
                 cookie1 = json.loads(cookie)
                 # data1 = {"chkSubmit": "ok", "txtLoginId": username, "txtPassword": password, "txtSel": 1}
-                r
+                # r
                 if len(token) > 1:
                     rq = requests.get(data00, cookies=cookie1)
                     content = rq.text
